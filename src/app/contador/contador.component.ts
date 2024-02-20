e4e6457c0236ae874f553719f629d51b9128785f
ESTADISTICA 05 <ESTADISTICA05@MINEDU.GOB.PE>
2024-02-20 17:58:48 -0500

    event binding listo

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-contador',
@@ -7,23 +8,28 @@ import { Component } from '@angular/core';
 })
 export class ContadorComponent {
 
-  constructor(){
+  constructor(){}
 
-  }
-
-  ngOnInit():void {
-
-  }
+  ngOnInit():void {}
 
   /*
   tipado flexible
   */
-  nombre:string = "REGPRO";
+  nombre:string = "REGPRO prueba";
   nombre2 = "REGPROAngular 15";
 
-  usuario = {
-    codoid : "40758607",
-    password: "123456789"
+  usuario:Persona = {
+    nombre : "40758607",
+    edad: 123456789
   };
 
+  numero:number = 1;
+
+  decrement() {
+    this.numero--;
+  }
+
+  increment() {
+    this.numero++;
+  }
 }
